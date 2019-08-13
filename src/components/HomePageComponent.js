b9435769b3dfd0446160a8e351f3dc4abfccc1e7
PaulElcampeon <pauloladele@live.co.uk>
2019-08-13 18:35:18 +0100

    added call to wakeup backend as soon as front end rendered

--- src/components/HomePageComponent.js
+++ src/components/HomePageComponent.js
@@ -1,38 +1,47 @@
-import React, {Component} from 'react';
-import {Redirect} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import '../App.css';
 
 
 class HomePage extends Component {
-    constructor() {
-        super();
-        this.state = {
-            redirect: false
-        }
+  constructor() {
+    super();
+    this.state = {
+      redirect: false
     }
+  }
 
-    setRedirect = () => {
-        this.setState({
-          redirect: true
-        })
-      }
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
 
-    renderRedirectToGamePage = () => {
-        if (this.state.redirect) {
-          return <Redirect to='/game' />
-        }
-      }
-
-    render() {
-
-        return (
-            <div className="text-white mainDiv">
-                {this.renderRedirectToGamePage()}
-                <h1 className="text-white mt-5 titleTextSize specialFont">Tic Tac Toe</h1>
-                <button className="textSize btnCs m-auto p-4 btnTextSize specialFont" onClick={this.setRedirect}>PLAY</button>
-            </div>
-        )
+  renderRedirectToGamePage = () => {
+    if (this.state.redirect) {
+      return <Redirect to='/game' />
     }
+  }
+
+  wakeUpBackEnd = () => {
+    fetch('https://tic-tac-toe-be.herokuapp.com')
+      .then((res) => { return res.json() })
+      .then((data) => {
+        console.log(data);
+      })
+  }
+
+  render() {
+
+    return (
+      <div className="text-white mainDiv">
+        {this.wakeUpBackEnd()}
+        {this.renderRedirectToGamePage()}
+        <h1 className="text-white mt-5 titleTextSize specialFont">Tic Tac Toe</h1>
+        <button className="textSize btnCs m-auto p-4 btnTextSize specialFont" onClick={this.setRedirect}>PLAY</button>
+      </div>
+    )
+  }
 
 }
 
